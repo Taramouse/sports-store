b66978f25d24b2b2bdba73d991b84dc4a097405e
taramouse <nickykeyse@gmail.com>
2019-08-06 16:19:25 +0100

    Changed location of router.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
