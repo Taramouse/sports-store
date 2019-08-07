9656ce82a2aa872e741cb510d2028025e1a0b607
taramouse <nickykeyse@gmail.com>
2019-08-07 20:20:35 +0100

    Added product search.

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -8,24 +8,34 @@
     </div>
     <div class="row">
       <div class="col-sm-12 col-md-3 bg-info p-2">
-        <category-controls />
+        <category-controls class="mb-5" />
+        <button class="btn btn-block btn-warning mt-5"
+                v-on:click="setShowSearch(true)">
+          Search
+        </button>
       </div>
       <div class="col-sm-12 col-md-9 p-2">
+        <search />
         <product-list />
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mapMutations } from 'vuex'
 import ProductList from '@/components/ProductList'
 import CategoryControls from '@/components/CategoryControls'
 import CartSummary from '@/components/CartSummary'
+import Search from '@/components/Search'
 
 export default {
   name: 'Store',
-  components: { ProductList, CategoryControls, CartSummary },
+  components: { ProductList, CategoryControls, CartSummary, Search },
   data () {
     return {}
+  },
+  methods: {
+    ...mapMutations(['setShowSearch'])
   }
 }
 </script>
